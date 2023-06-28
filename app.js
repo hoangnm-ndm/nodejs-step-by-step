daf9a56afbf5de880e5f8cf5d5111d49df51486b
hoangm.ndm <hoangm.ndm@gmail.com>
2023-06-28 22:49:12 +0700

    Step 2: Gửi đi dữ liệu dạng json

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end(`<h1>Xin chao moi nguoi!</h1>`);
+// import { createServer } from "http";
+// const server = createServer((req, res) => {
+//   res.end(`<h1>Xin chao moi nguoi!</h1>`);
+// });
+// server.listen(8088, () => {
+//   console.log("Server listening on port 8088");
+// });
+
+/**
+ * Khi dữ liệu trả về là json
+ */
+// import http from "http";
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(
+//     JSON.stringify({
+//       data: "Hello",
+//     })
+//   );
+// });
+
+// server.listen(8088, () => {
+//   console.log("Server listening on port 8088");
+// });
+
+/**
+ * Khi dùng express
+ */
+
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  // res.end(JSON.stringify({ id: "123", name: "product A", price: 200 }));
+  res.status(200).json({ id: "123", name: "product A", price: 200 });
 });
-server.listen(8088, () => {
-  console.log("Server listening on port 8088");
+app.listen(8088, () => {
+  console.log("server running on port 8088");
 });
